be52d1fc8f3d2085a12c7cf913c57446a1435910
France-Nor Brute <fbrute@gmail.com>
2014-11-23 11:21:48 -0400

    validation ok price, title, description and image_url

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
     validates :title, :description, :image_url, presence: true
-    validates :price, format: { :with => /\A\d+(?:\.\d{0,2})?\z/, message: I18n.t('two decimals please')}, numericality: {greater_than_or_equal_to: 0.01} 
+    #validates :price, numericality: {greater_than_or_equal_to: 0.01},  format: { with: /\A\d+(?:\.\d{0,2})?\z/, message: I18n.t('two decimals please')}  
+    #validates :price, numericality: {greater_than_or_equal_to: 0.01},  format: { with: /\.\d\d$\z/, message: I18n.t('two decimals please')}  
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}
     validates :title, uniqueness:true
     validates :image_url, allow_blank: true, format: {
         with: %r{\.(gif|jpg|png)\Z}i,
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,69 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+  
+  test "product price must be positive" do
+    product = Product.new(title:        "My Book Title",
+                          description:  "yyy",
+                          image_url:    "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["doit être supèrieur ou égal à 0.01"], product.errors[:price]
+
+    #assert_equal ": deux chiffres après la virgule s'il vous plaît", product.errors[:price][1]
+
+    product.price = 0 
+    assert product.invalid?
+    assert_equal ["doit être supèrieur ou égal à 0.01"], product.errors[:price]
+
+    #assert_equal ": deux chiffres après la virgule s'il vous plaît", product.errors[:price][1]
+
+
+    product.price = 1.00 
+    assert product.valid?
+    #assert_equal ": deux chiffres après la virgule s'il vous plaît", product.errors[:price]
+
+  end
+  
+  def new_product(image_url)
+    Product.new(title:        "My book Title",
+                description:  "yyy",
+                price:        1,
+                image_url:  image_url)
+  end
+  
+  test "image_url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+              http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.git.more }
+
+   ok.each do |name|
+     assert new_product(name).valid? "#{name} should be valid"
+   end
+
+   bad.each do |name|
+     assert new_product(name).invalid? "#{name} shouldn't be valid"
+   end
+ 
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title:        products(:ruby).title,
+                          description:  "yyy",
+                          price:        1,
+                          image_url:  "fred.gif")
+  assert product.invalid?
+  assert_equal [I18n.t('errors.messages.taken')], product.errors[:title]
+  end
+
 end
